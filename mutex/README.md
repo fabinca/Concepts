aaefd226e81707d44d6e558c5d960638e7071c4b
cfabian <cfabian@student.42wolfsburg.de>
2022-02-17 14:56:27 +0100

    mutex

---
+++ mutex/README.md
@@ -0,0 +1,7 @@
+# How to use mutex locks
+
+A small program to understand & try out the concept of pthreads and mutex locks in C.
+
+### Compile & run:                          
+> gcc *.c -lpthread -o mutex
+> ./mutex
---
+++ mutex/mutex.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mutex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cfabian <cfabian@student.42wolfsburg.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/17 13:29:37 by cfabian           #+#    #+#             */
+/*   Updated: 2022/02/17 14:53:35 by cfabian          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+typedef struct s_args
+{
+	int				i;
+	int				counter;
+	pthread_mutex_t	lock;
+}					t_args;
+
+void	*add(void *args)
+{
+	int	i;
+	int	j;
+
+	j = ((t_args *)args)->i;
+	usleep(j);
+	i = 0;
+	while (i++ < 10)
+	{
+		pthread_mutex_lock(&(((t_args *)args)->lock));
+		((t_args *)args)->counter++;
+		printf("Thread: %d, counter: %d\n", j, ((t_args *)args)->counter);
+		pthread_mutex_unlock(&(((t_args *)args)->lock));
+		usleep(1);
+	}
+	return (NULL);
+}
+
+int	main(void)
+{
+	pthread_t	tid[5];
+	t_args		*args;
+
+	args = malloc(sizeof(t_args));
+	args->i = -1;
+	args->counter = 0;
+	while (++(args->i) < 5)
+	{
+		pthread_create(&(tid[args->i]), NULL, &add, (void *)args);
+	}
+	pthread_join(tid[0], NULL);
+	pthread_join(tid[1], NULL);
+	pthread_join(tid[2], NULL);
+	pthread_join(tid[3], NULL);
+	pthread_join(tid[4], NULL);
+	return (0);
+}
